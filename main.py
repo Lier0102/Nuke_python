5444dea500fb90e240780696211d84b1d1380646
Lier0102 <75836412+Lier0102@users.noreply.github.com>
2022-12-04 11:56:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,16 @@ from utils.Setting.setup_most import * # 기본적인 설정들 로드
 import utils.Plugin.ServerChecker
 
 ############### 기능들 로드하기 ###############
+
+############### 디자인 관련 상수 설정 ###############
+
+w = Fore.WHITE
+b = Fore.BLACK
+lg = Fore.LIGHTGREEN_EX
+ly = Fore.LIGHTYELLOW_EX
+lm = Fore.LIGHTMAGENTA_EX
+lc = Fore.LIGHTCYAN_EX
+lr = Fore.LIGHTRED_EX
+lb = Fore.LIGHTBLUE_EX
+
+############### 디자인 관련 상수 설정 ###############
