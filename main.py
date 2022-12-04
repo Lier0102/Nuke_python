48507d1a4aa476c54ffd422911b5cbbf1ed308fd
Lier0102 <75836412+Lier0102@users.noreply.github.com>
2022-12-04 11:55:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,11 @@
 #############################
 
 # 프사 링크 : https://cdn.discordapp.com/attachments/1048422297385574430/1048453524268257330/images.jpeg
+
+from utils.Setting.setup_most import * # 기본적인 설정들 로드
+
+############### 기능들 로드하기 ###############
+
+import utils.Plugin.ServerChecker
+
+############### 기능들 로드하기 ###############
