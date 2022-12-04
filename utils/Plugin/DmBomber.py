f05de1c2c90d33b4999db9f5cb07c002b8892b7b
Lier0102 <75836412+Lier0102@users.noreply.github.com>
2022-12-04 09:32:59 +0900

    Create DmBomber.py

---
+++ utils/Plugin/DmBomber.py
@@ -0,0 +1,10 @@
+import requests
+
+# dm테러 기능을 사용하려면, 서버에 특정 토큰, 프록시로 
+# dm을 새로 생성할 때 만들어지는 요청을 제거해야됨.
+# 그러면 토큰이랑 채널(디엠에도 채널 아이디가 있음)아이디를 얻어야 하는데, 이건 인자로 둘 것임
+
+def DmBomber(token, channels):
+    for channel in channels:
+        requests.delete(f"https://discord.com/api/v7/channels/" + channel['id']) # 헤더를 전송해야 하는데, 아직 특정되지 않음.
+    
