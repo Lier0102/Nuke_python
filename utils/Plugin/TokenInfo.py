bf36cec92bd4d45ae910c7c751b632038e69d9a1
hhaewon <shinhae40@gmail.com>
2022-12-10 16:32:42 +0900

    implement TokenInfo factor

--- utils/Plugin/TokenInfo.py
+++ utils/Plugin/TokenInfo.py
@@ -1,3 +1,52 @@
-def get_token_info(token: str):
+import aiohttp
+import asyncio
+import datetime
+
+from colorama import Fore
+
+from utils.Setting.setup_most import heads, languages
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+def get_user_created_at(id: str) -> str:
+    KST = datetime.timezone(datetime.timedelta(hours=9))
+    timestamp = ((int(id) >> 22) + 1420070400000) / 1000
+    return datetime.datetime.fromtimestamp(timestamp, tz=KST).strftime("%Y-%m-%d %H:%M:%S")
+
+async def get_token_info(token: str):
+    premium_types = {
+        0: None,
+        1: "Nitro Classic",
+        2: "Nitro",
+        3: "Nitro Basic"
+    }
+    avatar_base_url = "https://cdn.discordapp.com/avatars/{id}/{hash}.webp?size=256"
     url = "https://discord.com/api/v10/users/@me"
     
+    async with (
+        aiohttp.ClientSession() as session,
+        await session.get(url=url, headers=heads(token=token)) as response
+    ):
+        response = await response.json()
+        
+    user_nitro_type = premium_types[response["premium_type"]]
+    if response["avatar"] is not None:
+        user_avatar_link = avatar_base_url.format(id=response["id"], hash=response["avatar"]) 
+    else:
+        user_avatar_link = None
+    created_at = get_user_created_at(response["id"])    
+    print(
+            f"""
+        {Fore.RESET}{Fore.GREEN}####### 토큰 정보 #######{Fore.RESET}
+        [{Fore.LIGHTMAGENTA_EX}이름{Fore.RESET}]             $:   {response['username']}#{response['discriminator']} 
+        [{Fore.LIGHTMAGENTA_EX}ID{Fore.RESET}]               $:   {response['id']}
+        [{Fore.LIGHTMAGENTA_EX}토큰{Fore.RESET}]             $:   {token}
+        [{Fore.LIGHTMAGENTA_EX}이메일{Fore.RESET}]           $:   {response["email"]}
+        [{Fore.LIGHTMAGENTA_EX}전화번호{Fore.RESET}]         $:   {response["phone"]}
+        [{Fore.LIGHTMAGENTA_EX}계정 생성일{Fore.RESET}]      $:   {created_at}
+        [{Fore.LIGHTMAGENTA_EX}지역 및 언어{Fore.RESET}      $:   {languages[response['locale']]}
+        [{Fore.LIGHTMAGENTA_EX}니트로 구매 여부{Fore.RESET}] $:   {user_nitro_type}
+        [{Fore.LIGHTMAGENTA_EX}이차인증 여부{Fore.RESET}]    $:   {response["mfa_enabled"]}
+        [{Fore.LIGHTMAGENTA_EX}아바타 주소{Fore.RESET}]      $:   {user_avatar_link}
+    """
+    )
