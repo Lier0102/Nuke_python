0c423be1d46195fda2d33a77d6f13556259af4f5
Lier0102 <75836412+Lier0102@users.noreply.github.com>
2022-12-10 17:20:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
 
 # 프사 링크 : https://cdn.discordapp.com/attachments/1048422297385574430/1048453524268257330/images.jpeg
 
-from utils.Setting.setup_most import *  # 기본적인 설정들 로드
 from utils.Setting.lib import *
+from utils.Setting.setup_most import *  # 기본적인 설정들 로드
 
 ############### 기능들 로드하기 ###############
 
