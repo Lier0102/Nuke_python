d6f78ff655bccdef5656f80f689ed92d15d82295
Lier0102 <75836412+Lier0102@users.noreply.github.com>
2022-12-10 16:34:33 +0900

    Update ServerChecker.py

--- utils/Plugin/ServerChecker.py
+++ utils/Plugin/ServerChecker.py
@@ -13,57 +13,53 @@ from utils.Setting.setup_most import *
 
 colorama.init(autoreset=True)
 
+def menu():
+    print(f"""
+[\x1b[95m1\x1b[95m\x1B[37m] 서버 체커
+[\x1b[95m2\x1b[95m\x1B[37m] 나가기
+""")
+menu()
 
-def menu():  # UI
-    print(
-        f"""
-        [\x1b[95m1\x1b[95m\x1B[37m] 서버 체크
-        [\x1b[95m2\x1b[95m\x1B[37m] 나가기
-        """)
-    global option
-    option = int(input(f"[\x1b[95m>\x1b[95m\x1B[37m] 선택 ㄱ: "))
+option = int(input(f"[\x1b[95m>\x1b[95m\x1B[37m] 옵션: "))
 
-
-def fetch():
-    menu()
+def fetch_data():
+        menu()
 if option == 1:
-    sleep(1)
+        sleep(1)
 
-    headers = {
-            "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7",
-            "Authorization": input(f"\n[\x1b[95m>\x1b[95m\x1B[37m] 토큰 입력: "),
-    }
+        headers = {
+            'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7',
+            'Authorization' : input(f"\n[\x1b[95m>\x1b[95m\x1B[37m] 토큰: ")
+        }
 
-    guildId = input(f"[\x1b[95m>\x1b[95m\x1B[37m] 서버 ID 입력: ")
+        guildId = input(f"[\x1b[95m>\x1b[95m\x1B[37m] 서버 ID: ")
 
-    response = requests.get(
-            f"https://discord.com/api/v10/guilds/{guildId}",
-            headers=headers,
-            params={"with_counts": True},
-    ).json()
+        response = requests.get(
+            f"https://discord.com/api/guilds/{guildId}",
+            headers = headers,
+            params = {"with_counts" : True}
+        ).json()
 
-    owner = requests.get(
-            f"https://discord.com/api/v10/guilds/{guildId}/members/{response['owner_id']}",
-            headers=headers,
-    ).json()
+        owner = requests.get(
+            f"https://discord.com/api/guilds/{guildId}/members/{response['owner_id']}",
+            headers = headers,
+            params = {"with_counts" : True}
+        ).json()
 
-    print(
-            f"""
-        {Fore.RESET}{Fore.GREEN}####### 서버 정보 #######{Fore.RESET}
-        [{Fore.LIGHTMAGENTA_EX}서버 이름{Fore.RESET}]      $:   {response['name']} 
-        [{Fore.LIGHTMAGENTA_EX}서버 ID{Fore.RESET}]        $:   {response['id']}
-        [{Fore.LIGHTMAGENTA_EX}서버 주인{Fore.RESET}]     $:   {owner['user']['username']}#{owner['user']['discriminator']} 
-        [{Fore.LIGHTMAGENTA_EX}서버 주인 ID{Fore.RESET}]  $:   {response['owner_id']}
-        [{Fore.LIGHTMAGENTA_EX}멤버수{Fore.RESET}]   $:   {response['approximate_member_count']}
-        [{Fore.LIGHTMAGENTA_EX}지역{Fore.RESET}]    $:   {response['region']}
-        [{Fore.LIGHTMAGENTA_EX}아이콘{Fore.RESET}]  $:   https://cdn.discordapp.com/icons/{guildId}/{response['icon']}.webp?size=256
-    """
-    )
-    sleep(6)
-    Hydron()
+        print(f"""
+{Fore.RESET}{Fore.GREEN}####### 서버 정보 #######{Fore.RESET}
+[{Fore.LIGHTMAGENTA_EX}서버이름{Fore.RESET}]      $:   {response['name']} 
+[{Fore.LIGHTMAGENTA_EX}서버ID{Fore.RESET}]        $:   {response['id']}
+[{Fore.LIGHTMAGENTA_EX}서버주인{Fore.RESET}]     $:   {owner['user']['username']}#{owner['user']['discriminator']} 
+[{Fore.LIGHTMAGENTA_EX}서버주인ID{Fore.RESET}]  $:   {response['owner_id']}
+[{Fore.LIGHTMAGENTA_EX}멤버수{Fore.RESET}]   $:   {response['approximate_member_count']}
+[{Fore.LIGHTMAGENTA_EX}지역{Fore.RESET}]    $:   {response['region']}
+[{Fore.LIGHTMAGENTA_EX}서버 아이콘{Fore.RESET}]  $:   https://cdn.discordapp.com/icons/{guildId}/{response['icon']}.webp?size=256
+""")
+        sleep(6)
+        Hydron()
 
 elif option == 2:
     Hydron()
-
-if __name__ == "__main__":
-    fetch()
+if __name__ == '__main__':
+        fetch_data()
