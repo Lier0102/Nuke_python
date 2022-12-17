472008a0016370b59191e5fcac0458b4a5937317
Lier0102 <75836412+Lier0102@users.noreply.github.com>
2022-12-17 20:57:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # https://github.com/Lier0102 && https://github.com/ShinHaewon
 # Coded / Dev : Lier0102 && ShinHaewon
 # Copyright © Hydron_Nuker
+# WEBSITE : Lier0102
 #############################
 
 # 프사 링크 : https://cdn.discordapp.com/attachments/1048422297385574430/1048453524268257330/images.jpeg
