db04a8ea7669ca67092699804e826bfae4337956
Lier0102 <75836412+Lier0102@users.noreply.github.com>
2022-12-18 19:56:42 +0900

    Update HypeChanger.py

--- utils/Plugin/HypeChanger.py
+++ utils/Plugin/HypeChanger.py
@@ -60,7 +60,7 @@ class HypeChanger(PluginABC):
             time.sleep(1)
             token = open('tokens.txt', 'r').read().splitlines()
             await TokenValidator(token[0])
-            
+            await cls.change(token[0]) # 아직은 완전한 버전이 아니라서 첫번째 토큰만 바꾸기
             
             input(f'\n[\x1b[95m>\x1b[95m\x1B[37m] 엔터를 눌러주세요: ')
             await importlib.import_module("main").Hydron()
