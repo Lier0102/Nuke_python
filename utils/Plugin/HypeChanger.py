0d42274ce86f4e1054068a320bd1fb89eed6557b
Lier0102 <75836412+Lier0102@users.noreply.github.com>
2022-12-18 20:03:21 +0900

    Update HypeChanger.py

--- utils/Plugin/HypeChanger.py
+++ utils/Plugin/HypeChanger.py
@@ -8,16 +8,14 @@ from utils.Setting.setup_most import TokenValidator, importlib
 class HypeChanger(PluginABC):
     plugin_name = "하이퍼스쿼드 변경"
 
-    print(f'''\n[\x1b[95m1\x1b[95m\x1B[37m] {Fore.MAGENTA}용맹(Bravery){Fore.RESET}
+    @classmethod
+    def change(token: str):
+
+        print(f'''\n[\x1b[95m1\x1b[95m\x1B[37m] {Fore.MAGENTA}용맹(Bravery){Fore.RESET}
 [\x1b[95m2\x1b[95m\x1B[37m] {Fore.LIGHTRED_EX}찬란함(Brilliance){Fore.RESET}
 [\x1b[95m3\x1b[95m\x1B[37m] {Fore.LIGHTCYAN_EX}균형(Balance){Fore.RESET}
 [\x1b[95m4\x1b[95m\x1B[37m] 하이퍼스쿼드 나가기''')
-
-    global house
-    house = input("\n[\x1b[95m>\x1b[95m\x1B[37m] 하이퍼스쿼드 선택: ")
-
-    @classmethod
-    def change(token: str):
+        house = input("\n[\x1b[95m>\x1b[95m\x1B[37m] 하이퍼스쿼드 선택: ")
         headers = mainHeader(token)
 
         if house == "1":
